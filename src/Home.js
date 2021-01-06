f54d8d5d97cb4c233b924b95cea80a89741a7837
crypt0 <acvjetan@gmail.com>
2021-01-06 13:23:40 +0100

    Reusable components.

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
+  const [blogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
@@ -10,7 +10,8 @@ const Home = () => {
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs}/>
+      <BlogList blogs={blogs} title="All blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" />
     </div> 
    );
 }
