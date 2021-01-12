e8715e81e5e7c3660ed6749bcf92375fda779e71
crypt096 <acvjetan@gmail.com>
2021-01-12 17:51:53 +0100

    Add handling errors.

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,28 @@ import BlogList from './BlogList'
 const Home = () => {
   const [blogs,setBlogs] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    const getData = async() => {
-      const res = await fetch('http://localhost:8000/blogs');
-      const data = await res.json();
+    fetch('http://localhost:8000/blogs').then(
+      res => {
+        if(!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json();
+      }
+    ).then(data=> {
       setBlogs(data);
-      setIsLoading(false)
-    }
-
-    getData();
-  },[])
+      setIsLoading(false);
+      setError(null);
+    }).catch(err => {
+      setIsLoading(false);
+      setError(err.message)
+    })},[]);
 
   return ( 
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div> 
