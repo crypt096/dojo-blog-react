85d96c56bc0d2338b4d89566fa552780d46067e7
crypt096 <acvjetan@gmail.com>
2021-01-18 11:46:10 +0100

    Add route parameters.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './Navbar';
 import Home from './Home'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -16,6 +18,9 @@ function App() {
           </Route>
           <Route path="/create">
             <Create/>
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails/>
           </Route>
         </Switch>
       </div>
